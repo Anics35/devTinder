822135bfed00b7432ba34ac53f6a17951335f3c6
Anics35 <sahaanirban278@gmail.com>
2025-03-07 12:11:58 +0530

    multiple_route_handlers

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require ("express");
 
 const app = express();
 
-app.get("/user", (req, res)=>{
-    // using ? -> b is optional
-    console.log(req.query);
-    
-    res.send({firstName: "Anirban", lastName:"Saha"});
-
+app.use("/user", (req, res, next)=>{
+    console.log("Handling the route user");
+    //res.send("Response")
+    next();
+},
+(req, res,next)=>{
+    console.log("Handling route user 2!");
+    next();
+   // res.send("3rd respose")
+   
+},
+(req, res,next)=>{
+    console.log("Handling route user 3!");
+    res.send("4th respose")
+   
 }
-)
 
+
+)
 
 
 app.listen(3000 , ()=>{
     console.log("Server is successfully listening on port 3000...");
-    });
+    });    
