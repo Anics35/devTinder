995a8d249d33d1465eaa392e7d4bd1d715e4c077
Anics35 <sahaanirban278@gmail.com>
2025-03-07 12:09:28 +0530

    multiple_route_handler

--- src/app.js
+++ src/app.js
@@ -2,47 +2,17 @@ const express = require ("express");
 
 const app = express();
 
-
-
-//request handler
-// app.use("/hello",(req,res)=>
-//     {
-//         res.send("Hello hello hello!")
-//     })
-
-
-//this will handle only GET call to /user
-app.get("/user",(req, res)=>{
-    res.send({firstName: "Anirban", lastName:"Saha"})
-} )
-
-
-//this will handle only POST call to /user
-app.post("/user",(req, res)=>{
- //  console.log("Save Data to database");
-   res.send("Data saved successfully")
-   
-} )
-
-app.delete("/user",(req, res)=>{
-    res.send("Deleted successfully")
-} )
-
-
-
-
-app.use("/test",(req,res)=>
-{
-    res.send("Hiii from the server!")
-})
-
-// app.use("/", (req, res)=>
-//     {
-//         res.send("Hello from /!")
-//     }
-//     )
-
+app.get("/user", (req, res)=>{
+    // using ? -> b is optional
+    console.log(req.query);
     
+    res.send({firstName: "Anirban", lastName:"Saha"});
+
+}
+)
+
+
+
 app.listen(3000 , ()=>{
     console.log("Server is successfully listening on port 3000...");
     });
