07227217f73205e526cfc6fd1bec9e7a60555d47
Anics35 <sahaanirban278@gmail.com>
2025-03-12 00:05:06 +0530

    Schema improvisation

--- src/app.js
+++ src/app.js
@@ -40,6 +40,51 @@ app.get("/user", async (req, res)=>{
   }
 })
 
+//FEED API- GET /feed - get all the users from the database
+app.get("/feed", async(req, res)=>{
+
+try{
+const users = await User.find({}) //gives back all users
+res.send(users);
+}
+catch(err){
+  res.status(400).send("Something went wrong")
+}
+
+})
+//deleting a user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    //const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Update failed "+ err.message);
+  }
+});
+
+
+
 
 connectDB().then(
   ()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,53 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
     {
         firstName: {
-            type : String
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 50,
         },
         lastName: {
-            type : String
+            type: String
         },
-        emailId:{
-            type:String
+        emailId: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
         },
-        password:{
-            type:String
+        password: {
+            type: String,
+            required: true
         },
-        age:{
-            type:Number
+        age: {
+            type: Number,
+            min: 18,
+
         },
-        gender:{
-            type:String
+        gender: {
+            type: String,
+            validate(value) {
+                if (!["male", "femaile", "others"].includes(value)) {
+                    throw new Error("Gender data not valid")
+                }
+            }
+        },
+        photoUrl: {
+            type: String,
+            default: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+        },
+        about: {
+            type: String,
+            default: "This is a default about of the user!"
+        },
+        skills: {
+            type: [String]
         }
+
+
+    },
+    {
+        timestamps: true,
     }
 );
 
